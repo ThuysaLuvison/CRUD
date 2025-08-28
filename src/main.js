169ff690e5cf695792a6d9d4a41a164f269dee8c
ThuysaLuvison <thuysaluv@hotmail.com>
2025-08-27 22:38:51 -0300

    Adicionado rotas de GET, PUT e DELETE

--- src/main.js
+++ src/main.js
@@ -10,19 +10,76 @@ dotenv.config();
 
 const PORT = process.env.PORT;
 app.post("/", async (request, response) => {
-  const { name, price, imageUrl } = request.body
+  const { name, price, imageUrl } = request.body;
 
   const product = await prisma.product.create({
     data: {
       name: name,
       price: price,
-      imageUrl: imageUrl
-    }
-  })
+      imageUrl: imageUrl,
+    },
+  });
 
-  return response.status(201).json(product, {message: "Produto cadastrado com sucesso!"})
+  return response
+    .status(201)
+    .json(product, { message: "Produto cadastrado com sucesso!" });
+});
+app.get("/", async (request, response) => {
+  const product = await prisma.product.findMany();
+  return response
+    .status(200)
+    .json(product, { message: "Produto listado com sucesso!" });
+});
+app.get("/:id", async (request, response) => {
+  const { id } = request.params;
+  try {
+    const product = await prisma.product.findUnique({
+      where: { id },
+    });
+    if (!product) {
+      return response.status(404).json({ message: "Produto não encontrado!" });
+    }
+
+    return response
+      .status(200)
+      .json({ message: "Produto encontrado com sucesso!", product });
+  } catch (error) {
+    return response.status(500).json({
+      message: "Erro ao buscar o produto!",
+      error: error.message,
+    });
+  }
+});
+app.put("/:id", async (request, response) => {
+  const { name, price, imageUrl } = request.body;
+  const product = await prisma.product.update({
+    where: {
+      id: request.params.id,
+    },
+    data: {
+      name: name,
+      price: price,
+      imageUrl: imageUrl,
+    },
+  });
+
+  return response
+    .status(200)
+    .json(product, { message: "Produto atualizado com sucesso!" });
 });
 
+app.delete("/:id", async (request, response) => {
+  const { name, price, imageUrl } = request.body;
+
+  const product = await prisma.product.delete({
+    where: {
+      id: request.params.id,
+    },
+  });
+  return response
+    .status(200)
+    .json(product, { message: "Produto excluido com sucesso!" });
+});
 app.listen(PORT, () => {
   console.log(`Servidor iniciado com sucesso na porta ${PORT}`);
 });
